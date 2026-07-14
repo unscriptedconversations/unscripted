ceaabe8958ee3f98925247ef591fd11c3722e967
unscriptedconversations <unscriptedconversations3@gmail.com>
2026-07-13 20:13:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+  import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from '../lib/supabase'
 import Logo from '../components/Logo'
